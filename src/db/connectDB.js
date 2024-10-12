5c245de93fb39eb1a0dae0a475b275ca6d02fa5a
MSI\Rahul Pawar <pawar.rahul1002@gmail.com>
2024-10-12 17:43:07 +0530

    connected with database

---
+++ src/db/connectDB.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log(
+      "\nMongoDB connected || DB Host ::  ",
+      connectionInstance.connection.host
+    );
+  } catch (err) {
+    console.log("MongoDB connection error", err);
+    process.exit(1);
+  }
+};
+
+
+export default connectDB;

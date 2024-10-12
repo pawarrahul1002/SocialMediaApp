09b6dcf6b12894bba04e2a821a7deabe8c787c0c
MSI\Rahul Pawar <pawar.rahul1002@gmail.com>
2024-10-12 19:33:31 +0530

    added configured cookie-parser and cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-dotenv.config();
+const app = express();
 
-console.log("hello"+ process.env.PORT);
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+app.use(cookieParser())
+
+export default app;
